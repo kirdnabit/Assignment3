a5335582915b9a522357ce32e6c9fdff3b3a068d
kirdnabit <andrik51735@gmail.com>
2025-09-23 22:00:30 -0400

    Update and rename Assignment3.java to MailRoom.java

--- src/Assignment3.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Assignment3{
-  public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-    
-    
-  }
-}
---
+++ src/MailRoom.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class MailRoom{
+  public boolean handleLetter(Letter letter, ArrayList<Mail> letters){
+    
+    if (letter)
+  }
+  
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Welcome to KSUPS\n");
+    int option = 0;
+    while (option !=6){
+      System.out.println("1- Send letter\n" +
+                        "2- Send flat\n" +
+                        "3- Send regular box\n" + 
+                        "4- Send live box\n" + 
+                        "5- Dispatch items\n" + 
+                        "6- Quit\n" + 
+                        "Enter option: ");
+      option = sc.nextInt();
+
+      switch(option){
+        case 1:
+          
+    }
+  }
+}
